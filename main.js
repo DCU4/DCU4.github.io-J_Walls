61e542471f0adc06027c73c3a7ef3e283547520b
DCU4 <DylanJConnor4@gmail.com>
2018-03-05 14:39:42 +0000

    Save my work

---
+++ main.js
@@ -0,0 +1,39 @@
+
+$(function(){	
+	var instance = new TypeIt('#text-rotate', {
+     strings: ["creator.", "producer.", "designer.", "human."],
+     speed: 150,
+     breakLines: false,
+     autoStart: false,
+     lifeLike:false,
+     loop:true,
+     nextStringDelay:2000,
+     loopDelay:2000
+});
+
+
+$(".music-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#music").offset().top
+    }, 2000);
+});
+
+$(".about-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
+    }, 2000);
+});
+
+
+$(".contact-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top
+    }, 2000);
+});
+
+$(".front-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#front-page").offset().top
+    }, 2000);
+});
+});
